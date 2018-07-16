83ec38ea74fe9f8518ea1c3a1ddbdace9233c71e
mgolois <mgolois@yahoo.com>
2018-07-16 16:05:08 -0400

    Add project files.

---
+++ RequestClassifier.ML.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RequestClassifier.ML.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
